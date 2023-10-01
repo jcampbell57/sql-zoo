ad807aec9084b43303131b02bc9fe2a58443675f
jcampbell57 <jcampbell57@gmail.com>
2023-09-30 19:46:13 -0700

    update

--- 9-_window_function.sql
+++ 9-_window_function.sql
@@ -2,7 +2,13 @@
 
 -- 1. Show the lastName, party and votes for the constituency 'S14000024' in 2017.
 
+SELECT lastName, party, votes
+  FROM ge
+  WHERE constituency = 'S14000024' AND yr = 2017
+  ORDER BY votes DESC;
 
+
+-- The remaining questions are not currently functioning on the SQL Zoo website.
 
 -- 2. Show the party and RANK for constituency S14000024 in 2017. List the output by party
 
